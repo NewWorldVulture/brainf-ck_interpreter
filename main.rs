ecbb434f8e89c2183c0f5bfb3fe54898eff7a32b
NewWorldVulture <49806198+NewWorldVulture@users.noreply.github.com>
2019-09-19 04:37:17 -0700

    Updated main.rs
    updated with printing out functions being called.

--- main.rs
+++ main.rs
@@ -1,21 +1,29 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::env;
-use std::num::Wrapping;
 
 fn tokenize_char(x:&char) -> &str {
     match x {
-        '>' => "mv rit",
-        '<' => "mv let",
+        '>' => { //mv::rit();
+            "mv::rit()" },
+        '<' => { //mv::lft();
+            "mv::lft()" },
 
-        '+' => "op add",
-        '-' => "op sub",
+        '+' => { //op::add();
+            "op::add()" },
+        '-' => { //op::sub{};
+            "op::sub{}" },
 
-        ',' => "io get",
-        '.' => "io put",
+        ',' => { //io::get();
+            "io::get()" },
+        '.' => { //io::prt();
+            "io::prt()" },
 
-        '[' => "lp str",
-        ']' => "lp end",
+        '[' => { //lp::str();
+            "lp::str()" },
+        ']' => { //lp::end();
+            "lp::end()" },
+            
         _ => "",
     }
 }
@@ -25,6 +33,7 @@ fn main() -> std::io::Result<()> {
     // Read filename from CLI
     let args:Vec<String> = env::args().collect();
     let filename = &args[1];
+    //let input = &args[2];
 
     // Attempt to open and read the contents of the file
     let mut file = File::open(filename)?;
@@ -33,8 +42,8 @@ fn main() -> std::io::Result<()> {
 
     // Iterate over characters in program
     for i in contents.chars() {
-        println!("{}", i)
-
+        println!("{} : {}", i, tokenize_char(&i));
     }
+    
     Ok(())
 }
