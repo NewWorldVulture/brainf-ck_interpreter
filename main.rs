a9daf13f1ae910987da98d0f8a22ae7755d6d381
NewWorldVulture <49806198+NewWorldVulture@users.noreply.github.com>
2019-09-20 01:53:10 -0700

    Update main.rs
    Fixed previous compile errors
    Implemented the machine state as a struct as opposed to disparate data
    Standardized i/o for functions

--- main.rs
+++ main.rs
@@ -1,30 +1,38 @@
 use std::fs::File;
 use std::io::prelude::*;
-use std::env;
+use std::{ env, mem };
 
-fn tokenize_char(x:&char) -> &str {
-    match x {
-        '>' => { //mv::rit();
-            "mv::rit()" },
-        '<' => { //mv::lft();
-            "mv::lft()" },
+pub mod tape;
+use tape::State;
+
+pub mod mv;
+pub mod op;
+pub mod io;
+pub mod lp;
+
+fn parse_char(instruction:&char, state:&State) -> State {
+    match instruction {
+        '>' => { mv::rit(&mut state);
+            *state },
+        '<' => { mv::lft(&mut state);
+            *state },
 
         '+' => { //op::add();
-            "op::add()" },
+            *state },
         '-' => { //op::sub{};
-            "op::sub{}" },
+            *state },
 
-        ',' => { //io::get();
-            "io::get()" },
-        '.' => { //io::prt();
-            "io::prt()" },
+        ',' => { //io::get();   // Accept input
+            *state },
+        '.' => { io::prt(&mut state);   // Outputs character
+            *state },
 
         '[' => { //lp::str();
-            "lp::str()" },
+            *state },
         ']' => { //lp::end();
-            "lp::end()" },
-            
-        _ => "",
+            *state },
+
+        _ => *state,    // Skip anything that isn't a recognized instruction
     }
 }
 
@@ -37,13 +45,24 @@ fn main() -> std::io::Result<()> {
 
     // Attempt to open and read the contents of the file
     let mut file = File::open(filename)?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
+    let mut f_contents = String::new();
+    file.read_to_string(&mut f_contents)?;
+    // Cast `contents` to a vec, then forget the String
+    let program: Vec<char> = f_contents.chars().collect();
+    mem::forget(f_contents);
+
+    let mut state = State {
+        tape: [0].to_vec(),
+        ptr: 0u32,
+    };
 
     // Iterate over characters in program
-    for i in contents.chars() {
-        println!("{} : {}", i, tokenize_char(&i));
+    while (state.ptr as usize) < program.len() {
+        let instruction = &program[state.ptr as usize];
+        state = parse_char(&instruction, &mut state);
+        //println!("{} : {}", ptr, x);
+        state.ptr += 1;
     }
-    
+
     Ok(())
 }
