38edf39b43600187b98c118a34d278c983d2ce02
NewWorldVulture <49806198+NewWorldVulture@users.noreply.github.com>
2019-09-20 01:50:56 -0700

    Update mv.rs
    Implemented real functionality of mv.rs

--- mv.rs
+++ mv.rs
@@ -1,5 +1,13 @@
-pub fn rit() {
+//use self::State;
+
+pub fn rit(state:&State) -> State {
+    // Check if ptr goes above tape.len()
+    state.ptr + 1;
+    *state
 }
 
-pub fn lft() {
+pub fn lft(state:&State) -> State {
+    // Check if ptr drops below 0. If so, panic
+    state.ptr - 1;
+    *state
 }
